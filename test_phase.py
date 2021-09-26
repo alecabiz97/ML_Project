c86e12767946d8a8bfdc2970a69748c16a78dc74
alecabiz97 <alecabiz97@gmail.com>
2021-09-26 12:12:01 +0200

    In test_phase.py are run all the test for all the models trained. All the results are collected into a dictionary and saved in a file

--- test_phase.py
+++ test_phase.py
@@ -1,26 +1,10 @@
 import pickle
-import numpy as np
 import torch
-import torchvision
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader, Subset
 import torchvision.transforms as transforms
 from icub_datasets import ICubWorld28
 from tqdm import tqdm
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-train_on_gpu = torch.cuda.is_available()
-
-if train_on_gpu:
-    print("CUDA is available! Training on GPU...")
-else:
-    print("CUDA is not available. Training on CPU...")
-
-root = "iCubWorld28"
-
 
 def test(model, test_dataloader, criterion):
     model.eval()
@@ -52,36 +36,48 @@ def test(model, test_dataloader, criterion):
 
 if __name__ == "__main__":
 
-    model_name = "alexnet"  # Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
-    batch_size = 64  # Batch size for training (change depending on how much memory you have)
-    learning_rate = 0.001
+    root = "iCubWorld28"
+    results={}
+    for model_name in ['resnet', 'alexnet', 'squeezenet']:
+        for learning_rate in [0.01, 0.001, 0.0001]:
+            batch_size = 64  # Batch size for training (change depending on how much memory you have)
 
-    if model_name == 'inception':
-        input_size = 299
-    else:
-        input_size = 224
+            if model_name == 'inception':
+                input_size = 299
+            else:
+                input_size = 224
 
-    # Load the model
-    f = open(f'{model_name}_{learning_rate}.pkl', 'rb')
-    # f = open(f'{model_name}.pkl', 'rb')
-    model = pickle.load(f)
+            if torch.cuda.is_available():
+                print("CUDA is available! Training on GPU...")
+                device = "cuda"
+            else:
+                print("CUDA is not available. Training on CPU...")
+
+            # Load the model
+            f = open(f'{model_name}_{learning_rate}.pkl', 'rb')
+            model = pickle.load(f)
+            f.close()
+
+            model.to(device)
+
+            data_transforms = transforms.Compose([
+                transforms.Resize(input_size),
+                transforms.CenterCrop(input_size),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+            test_data = ICubWorld28(root, train=False, transform=data_transforms)
+            # rand_indx = np.random.permutation(np.arange(len(test_data)))[0:800]
+            # test_data = Subset(test_data, rand_indx)
+
+            test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+            criterion = nn.CrossEntropyLoss()
+
+            test_loss, test_accuracy = test(model, test_dataloader, criterion)
+            filename=f'{model_name}_{learning_rate}'
+            results[filename]=(test_loss, test_accuracy)
+
+    # Save the results
+    f=open('results.pkl','wb')
+    pickle.dump(results,f)
     f.close()
-
-    # Move tensors to GPU is CUDA is available
-    if train_on_gpu:
-        model.cuda()
-
-    data_transforms = transforms.Compose([
-        transforms.Resize(input_size),
-        transforms.CenterCrop(input_size),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-
-    test_data = ICubWorld28(root, train=False, transform=data_transforms)
-    # rand_indx = np.random.permutation(np.arange(len(test_data)))[0:800]
-    # test_data = Subset(test_data, rand_indx)
-
-    test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
-    criterion = nn.CrossEntropyLoss()
-
-    test_loss, test_accuracy = test(model, test_dataloader, criterion)
