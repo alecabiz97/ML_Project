e5f57d77038fe1e379fdfe7bd7fc25f3f9d9f363
alecabiz97 <alecabiz97@gmail.com>
2021-09-26 18:41:36 +0200

    In the function test I have added the evaluation of the confusion matrix. test return the confusion matrix and the accuracy during the test. The results for all the models are saved into a dictionary.

--- test_phase.py
+++ test_phase.py
@@ -5,37 +5,49 @@ from torch.utils.data import DataLoader, Subset
 import torchvision.transforms as transforms
 from icub_datasets import ICubWorld28
 from tqdm import tqdm
+from torchmetrics import ConfusionMatrix
+import numpy as np
 
-def test(model, test_dataloader, criterion):
+def test(model, test_dataloader, criterion,n_classes):
     model.eval()
 
     running_loss = 0
     correct = 0
     total = 0
 
+    # For confusion matrix
+    preds=[]
+    targets=[]
+
     with torch.no_grad():
         for data in tqdm(test_dataloader):
             images, labels = data[0].to(device), data[1].to(device)
 
+            targets.extend(labels.tolist())
             outputs = model(images)
 
             loss = criterion(outputs, labels)
             running_loss += loss.item()
 
             _, predicted = outputs.max(1)
+            preds.extend(predicted.tolist())
             total += labels.size(0)
             correct += predicted.eq(labels).sum().item()
 
-    test_loss = running_loss / len(test_dataloader)
+    #test_loss = running_loss / len(test_dataloader)
     accuracy = 100. * correct / total
 
-    print('\nTest Loss: %.3f | Accuracy: %.3f' % (test_loss, accuracy))
+    conf=ConfusionMatrix(num_classes=n_classes,normalize='true')
+    confusion_matrix=conf(torch.tensor(preds),torch.tensor(targets))
 
-    return test_loss, accuracy
+    print('\nTest Accuracy: %.3f' % (accuracy))
+
+    return confusion_matrix, accuracy
 
 
 if __name__ == "__main__":
 
+    num_classes = 7
     root = "iCubWorld28"
     results={}
     for model_name in ['resnet', 'alexnet', 'squeezenet']:
@@ -67,15 +79,15 @@ if __name__ == "__main__":
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
             test_data = ICubWorld28(root, train=False, transform=data_transforms)
-            # rand_indx = np.random.permutation(np.arange(len(test_data)))[0:800]
+            # rand_indx = np.random.permutation(np.arange(len(test_data)))[0:20]
             # test_data = Subset(test_data, rand_indx)
 
             test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
             criterion = nn.CrossEntropyLoss()
 
-            test_loss, test_accuracy = test(model, test_dataloader, criterion)
+            confusion_matrix, test_accuracy = test(model, test_dataloader, criterion,num_classes)
             filename=f'{model_name}_{learning_rate}'
-            results[filename]=(test_loss, test_accuracy)
+            results[filename]=(confusion_matrix, test_accuracy)
 
     # Save the results
     f=open('results.pkl','wb')
