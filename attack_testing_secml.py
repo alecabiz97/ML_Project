73a79e1346978b9959f0afd199a6c275e796f57a
gianl <gianlucapala98@gmail.com>
2021-10-13 11:43:35 +0200

    Modified attack_testing_secml.py by changing the image transform part, modified the attack parameters and evaluated the results based on 100 iterations. Defined a range of the epsilon used from 0 to 0.3

--- attack_testing_secml.py
+++ attack_testing_secml.py
@@ -4,13 +4,7 @@ from torch import optim
 from secml.ml.classifiers import CClassifierPyTorch
 from secml.ml.peval.metrics import CMetricAccuracy
 
-# these are required for the dataset and preprocessing
-from secml.data.loader import CDLRandomBlobs
-from secml.data.splitter import CTrainTestSplit
-from secml.ml.features import CNormalizerMinMax
-
 # these are used for the adversarial attacks
-from secml.optim.constraints import CConstraintL2
 from secml.array import CArray
 from secml.adv.attacks.evasion import CFoolboxPGDL2
 from secml.adv.seceval import CSecEval
@@ -22,61 +16,71 @@ from torch.utils.data import DataLoader
 
 # this is for visualization
 from secml.figure import CFigure
-from utils import run_debug,make_subset
+from utils import run_debug, make_subset
 from secml.data import CDataset
 from time import time
+from secml.ml.features import CNormalizerMeanStd
 
-n_samples_for_class=100
-sub_label=False
-#Perturbation levels to test
-e_vals = CArray.arange(start=0, step=50, stop=500)
+n_samples_for_class = 100
+sub_label = False
+# Perturbation levels to test
+e_vals = CArray.arange(start=0, step=0.05, stop=0.35)
 
-for modelname,lr in zip(['squeezenet1_0','resnet152','densenet161'],['0.01','0.01','0.001']):
-    f=open(f'trained_models/{modelname}_{lr}_7_classes.pkl','rb')
+for modelname, lr in zip(['squeezenet1_0', 'resnet152', 'densenet161'], ['0.01', '0.01', '0.001']):
+    f = open(f'trained_models/{modelname}_{lr}_7_classes.pkl', 'rb')
     model = pickle.load(f)
     f.close()
 
     data_transforms = transforms.Compose([
         transforms.Resize(224),
         transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+        transforms.ToTensor()])
+
+    normalizer = CNormalizerMeanStd(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+
     dataset = ICubWorld28('ICubWorld28', train=False, transform=data_transforms, sub_label=sub_label)
     labels = dataset.labels
-    X, Y = make_subset(dataset,n_samples_for_class)
-    print('Number of samples: ',len(Y))
+    X, Y = make_subset(dataset, n_samples_for_class)
+    print('Number of samples: ', len(Y))
     X, Y = CArray(X.numpy()), CArray(Y.numpy())
-    ts=CDataset(X,Y)
-    clf = CClassifierPyTorch(model=model, input_shape=(3, 224, 224), pretrained=True, pretrained_classes=labels)
+    ts = CDataset(X, Y)
+    clf = CClassifierPyTorch(model=model,
+                             input_shape=(3, 224, 224),
+                             preprocess=normalizer,
+                             pretrained=True,
+                             pretrained_classes=labels)
 
-    steps = 10
-    epsilon = 300
+    steps = 100
     y_target = None  # None if `error-generic`, the label of the target class for `error-specific`
     lb = 0.0
     ub = 1.0
+    abs_stepsize = 0.03
 
     pgd_attack = CFoolboxPGDL2(clf, y_target,
                                lb=lb, ub=ub,
-                               epsilons=epsilon,
-                               abs_stepsize=0.03,
+                               abs_stepsize=abs_stepsize,
                                steps=steps,
                                random_start=False)
-    #run_debug(clf,X,Y,pgd_attack)
 
+    # run_debug(clf,X,Y,pgd_attack)
 
-    sec_eval = CSecEval(
-        attack=pgd_attack, param_name='epsilon', param_values=e_vals)
+    sec_eval = CSecEval(attack=pgd_attack, param_name='epsilon', param_values=e_vals)
 
     # Run the security evaluation using the test set
-    start=time()
+    start = time()
     print("Running security evaluation...")
     sec_eval.run_sec_eval(ts)
 
     fig = CFigure(height=6, width=6)
 
     # Convenience function for plotting the Security Evaluation Curve
-    fig.sp.plot_sec_eval(sec_eval.sec_eval_data, marker='o', label='NN', show_average=True)
+    fig.sp.plot_sec_eval(sec_eval.sec_eval_data, marker='o', label=f'{modelname}',
+                         show_average=True, percentage=True)
+    fig.sp.ylim(0, 100)
+    fig.sp.ylabel("Accuracy (%)")
     CFigure.show()
-    end=time()
-    print('Evaluation time: {} minutes'.format(round((end-start)/60,2)))
-    fig.savefig(f'Security Evaluation Curve {modelname}_{lr}.pdf',file_format='pdf')
+    end = time()
+    print('Evaluation time: {} minutes'.format(round((end - start) / 60, 2)))
+
+    fig.savefig(f'Security Evaluation Curve {modelname} Learning Rate: {lr} Abs. Step Size: {abs_stepsize} '
+                f'Iterations: {steps}.pdf', file_format='pdf')
